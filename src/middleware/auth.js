9de4c994328724fca925facc5f5e73be399a6d51
saikumarkusangi <saikumarkusangi007@gmail.com>
2023-09-09 20:36:00 +0530

    express-async-handler removed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,8 @@
 
 import jwt from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
 import userModel from '../models/userModel.js';
 
-const authMiddleware = asyncHandler(async (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
     let token;
     if (req.headers && req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(" ")[1];
@@ -20,15 +19,15 @@ const authMiddleware = asyncHandler(async (req, res, next) => {
     } else {
         throw new Error('Access Token missing');
     }
-});
+};
 
 
-const isAdmin = asyncHandler(async (req, res, next) => {
+const isAdmin = async (req, res, next) => {
     const { userType } = req.user;
     if (userType !== 'Admin') {
         throw new Error("forbidden");
     }
     next();
-});
+};
 
 export { authMiddleware, isAdmin};
